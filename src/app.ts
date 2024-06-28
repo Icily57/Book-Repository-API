566eeb8287f39cd55cea1e4c31b3edddbfacd932
Icily57 <icilyweird57@gmail.com>
2024-06-28 10:57:57 +0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono';
-import { bookRoutes } from './Route/book.route';
+import { bookRouter } from './Route/book.route';
 
 const app = new Hono();
 
-app.route('/api/books', bookRoutes);
+app.route('/api/books', bookRouter);
 
 export { app };
